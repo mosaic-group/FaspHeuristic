cdd585708bfe683a080810f8187c2ecd0a4301e6
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2019-12-06 11:39:32 +0100

    Memory alloc test

--- src/graph/graphFasp.h
+++ src/graph/graphFasp.h
@@ -15,6 +15,85 @@
 
 namespace Graph::Fasp {
 
+    template <class T>
+    class MyAlloc {
+
+//#ifdef ROUTING_KIT_NO_ALIGNED_ALLOC
+        void*aligned_alloc(uint8_t alignment, uint64_t size){
+	uint64_t potentially_unaligned_buffer = (uint64_t)malloc(size+alignment);
+	uint64_t aligned_buffer = ((potentially_unaligned_buffer + alignment)/alignment) * alignment;
+	uint8_t*buffer = (uint8_t*)aligned_buffer;
+	*(buffer-1) = aligned_buffer - potentially_unaligned_buffer;
+	return buffer;
+}
+
+void aligned_free(void* ptr){
+	if(ptr){
+		uint8_t*buffer = (uint8_t*)ptr;
+		uint8_t shift = *(buffer-1);
+		buffer -= shift;
+		free(buffer);
+	}
+}
+//#endif
+    public:
+        // type definitions
+        typedef T        value_type;
+        typedef T*       pointer;
+        typedef const T* const_pointer;
+        typedef T&       reference;
+        typedef const T& const_reference;
+        typedef std::size_t    size_type;
+        typedef std::ptrdiff_t difference_type;
+
+        // rebind allocator to type U
+        template <class U>
+        struct rebind { typedef MyAlloc<U> other; };
+
+        // return address of values
+        pointer address (reference value) const { return &value; }
+        const_pointer address (const_reference value) const { return &value; }
+
+        // constructors and destructor - nothing to do because the allocator has no state
+        MyAlloc() throw() {}
+        MyAlloc(const MyAlloc&) throw() {}
+        template <class U> MyAlloc (const MyAlloc<U>&) throw() {}
+        ~MyAlloc() throw() {}
+
+        // return maximum number of elements that can be allocated
+        size_type max_size () const throw() { return std::numeric_limits<std::size_t>::max() / sizeof(T); }
+
+        // allocate but don't initialize num elements of type T
+        pointer allocate (size_type num, const void* = 0) {
+            // print message and allocate memory with global new
+//            std::cerr << "allocate " << num << " element(s)" << " of size " << sizeof(T) << std::endl;
+//            pointer ret = (pointer)(::operator new(num*sizeof(T)));
+            pointer ret = (pointer)(aligned_alloc(64, num*sizeof(T)));
+//            std::cerr << " allocated at: " << (void*)ret << std::endl;
+            return ret;
+        }
+
+        // initialize elements of allocated storage p with value value
+        void construct (pointer p, const T& value) {
+            // initialize memory with placement new
+            new((void*)p)T(value);
+        }
+
+        // destroy elements of initialized storage p
+        void destroy (pointer p) {
+            // destroy objects by calling their destructor
+            p->~T();
+        }
+
+        // deallocate storage p of deleted elements
+        void deallocate (pointer p, size_type num) {
+            // print message and deallocate memory with global delete
+//            std::cerr << "deallocate " << num << " element(s)" << " of size " << sizeof(T) << " at: " << (void*)p << std::endl;
+//            ::operator delete((void*)p);
+            aligned_free((void*)p);
+        }
+    };
+
     template <typename VERTEX_TYPE>
     using EdgesSet = std::unordered_set<typename Graph<VERTEX_TYPE>::Edge, Ext::EdgeHasher<VERTEX_TYPE>>;
 
@@ -31,9 +110,9 @@ namespace Graph::Fasp {
         DynamicBitset <uint32_t, VERTEX_TYPE> iVisited;
         Stack <VERTEX_TYPE> stack;
         std::vector<VERTEX_TYPE> parents;
-        std::vector<VERTEX_TYPE> lowLinks;
-        std::vector<VERTEX_TYPE> index;
-        std::vector<VERTEX_TYPE> path;
+        std::vector<VERTEX_TYPE, MyAlloc<VERTEX_TYPE>> lowLinks;
+        std::vector<VERTEX_TYPE,  MyAlloc<VERTEX_TYPE>> index;
+        std::vector<VERTEX_TYPE,  MyAlloc<VERTEX_TYPE>> path;
 
     public:
         explicit GraphSpeedUtils(std::size_t aN) : iVisited(aN), stack(aN), parents(aN, 0), lowLinks(aN, -1), index(aN, -1) {
