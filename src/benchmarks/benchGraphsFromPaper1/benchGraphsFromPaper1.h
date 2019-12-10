2d5d158294c4aed42b2794ebd02dc714a614c1b7
Krzysztof Gonciarz <gonciarz@mpi-cbg.de>
2019-12-10 17:45:13 +0100

    Benchmark for graphs from 'paper' cleaned up

--- src/benchmarks/benchGraphsFromPaper1/benchGraphsFromPaper1.h
+++ src/benchmarks/benchGraphsFromPaper1/benchGraphsFromPaper1.h
@@ -1,8 +1,3 @@
-//
-// Created by gonciarz on 2019-05-28.
-//
-
-
 #ifndef GRAPHS_FROM_PAPER_1
 #define GRAPHS_FROM_PAPER_1
 
@@ -18,8 +13,7 @@
 
 /*
  * Benchmarks of graphs from this paper:
- *
- * An exact method for the minimum feedback arc set problem
+ * "An exact method for the minimum feedback arc set problem" Ali Baharev et al.
  */
 
 std::string getFilenameOfBenchGraphsFromPaper1() {
@@ -28,51 +22,58 @@ std::string getFilenameOfBenchGraphsFromPaper1() {
 
 
 void benchGraphsFromPaper1(const std::string &outputDir, const std::string &inputDir) {
-
     LOG(TRACE) << "Running benchGraphsFromPaper1";
 
-    std::regex rx(R"(.*opt=([0-9]+).*)");
-    std::smatch match;
-
+    // create file for results
     auto outputFile = outputDir + "/" + getFilenameOfBenchGraphsFromPaper1();
-    DataHdf5<double> f(outputFile, /* create output file (dummy run) */ (outputDir == "" ? true : false));
+    DataHdf5<double> resultsFile(outputFile, /* create output file (dummy run) */ (outputDir == "" ? true : false));
 
+    // Simple statistics
     int numOfCorrect = 0;
     int numOfGraphs = 0;
     int largestError = 0;
+
     for (auto &file : Graph::IO::getFilesInDir(inputDir)) {
         if (!Tools::endsWith(file, "graph.txt")) continue;
 
         LOG(INFO) << " ======================= Processing [" << file << "] ";
 
+        // Extract exact FASP size from filename
+        std::regex rx(R"(.*opt=([0-9]+).*)");
+        std::smatch match;
         std::regex_match(file, match, rx);
-        int correct = 0;
-        if (match.size() != 2) LOG(ERROR) << "Cannot extract correct solution from file name! [" << file << "]";
+        int exactSolution = 0;
+        if (match.size() != 2) {
+            LOG(ERROR) << "Cannot extract exact solution from file name! [" << file << "]";
+            continue;
+        }
         else {
             std::ssub_match sub_match = match[1];
             std::string num = sub_match.str();
-            correct = std::stoi(num);
+            exactSolution = std::stoi(num);
         }
 
+        // Read graph from file and run ISO-CUT
         Graph::Graph gv = Graph::IO::graphFromFile<int>(inputDir + "/" + file);
-        auto c = Graph::Ext::getEdgeProperties<int>(gv, 1);
+        auto [capacity, removedEdges, tightCutEdgesCnt, tightCutRndEdgesCnt, redEdgesRndEdgesCnt] = Graph::Fasp::tightCut(gv);
 
-        auto [capacity, removedEdges, saEdgesCnt, saRndEdgesCnt, redRndEdgesCnt] = Graph::Fasp::tightCut(gv, c);
-        f.put("random", capacity);
-        f.put("exact", correct);
-        f.put("saEdges", saEdgesCnt);
-        f.put("saRndEdges", saRndEdgesCnt);
-        f.put("redRndEdges", redRndEdgesCnt);
+        // Save data
+        resultsFile.put("random", capacity);
+        resultsFile.put("exact", exactSolution);
+        resultsFile.put("saEdges", tightCutEdgesCnt);
+        resultsFile.put("saRndEdges", tightCutRndEdgesCnt);
+        resultsFile.put("redRndEdges", redEdgesRndEdgesCnt);
 
-        if (capacity == correct) ++numOfCorrect;
+        // Some statistics...
+        if (capacity == exactSolution) ++numOfCorrect;
         else {
             LOG(ERROR) << "Not exact soltion found!";
-            if (capacity - correct > largestError) largestError = capacity - correct;
+            if (capacity - exactSolution > largestError) largestError = capacity - exactSolution;
         }
         ++numOfGraphs;
     }
 
-    f.save();
+    resultsFile.save();
 
     LOG(INFO) << "Results - #graphs exactly solved / #graphs: " << numOfCorrect << "/" << numOfGraphs;
     LOG(INFO) << "Largest error: " << largestError;
